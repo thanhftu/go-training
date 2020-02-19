a02d98fcbbe94eda0932b345ec6a6a8461c29a6f
Thanh Truong <thanhtruong@Thanhs-MacBook-Pro.local>
2020-02-19 19:47:49 +0700

    add 03_variables

---
+++ 03_variables/01_shorthand/01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := "Golang"
+	c := 4.17
+	d := true
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+}
